7a25ee6a25b8f61c4fd204b7e6f3666f3baa961e
laremund <larsgremund@gmail.com>
2026-02-05 15:36:57 -0700

    Add pencil icon images and update App component to use new icon

--- src/App.tsx
+++ src/App.tsx
@@ -172,10 +172,7 @@ function App() {
                             className="hover:opacity-70 transition"
                             aria-label="Edit"
                           >
-                            <svg className="w-5 h-5 cursor-pointer" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                              <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                              <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
-                            </svg>
+                            <img src="/pencil_icon.png" alt="" className="w-6 h-6 cursor-pointer" />
                           </button>
                           {/* Delete Icon */}
                           <button
@@ -183,7 +180,7 @@ function App() {
                             className="hover:opacity-70 transition"
                             aria-label="Delete"
                           >
-                            <svg className="w-5 h-5 cursor-pointer" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                            <svg className="w-6 h-6 cursor-pointer" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                               <polyline points="3 6 5 6 21 6" />
                               <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
                             </svg>
