ceba808f4656ca7ac7f4e8ed1a9cec934555843c
laremund <larsgremund@gmail.com>
2026-02-05 16:17:05 -0700

    Replace delete SVG icon with new trash icon image in App component and add trash icon image file.

--- src/App.tsx
+++ src/App.tsx
@@ -210,10 +210,7 @@ function App() {
                                 className="hover:opacity-70 transition"
                                 aria-label="Delete"
                               >
-                                <svg className="w-6 h-6 cursor-pointer" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                                  <polyline points="3 6 5 6 21 6" />
-                                  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-                                </svg>
+                                <img src="/trash_icon.png" alt="" className="w-6 h-6 cursor-pointer" />
                               </button>
                             </div>
                           )}
